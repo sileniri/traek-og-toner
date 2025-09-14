e76b1679984002ba866a3a7f2078819d1522328a
SLNR <sigurrinnan@gmail.com>
2025-09-14 20:29:40 +0200

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/",
     plugins: [react()],
+    base: "/", // Ensure this points to the correct base
+    server: {
+        open: true,
+        // Configure server to return index.html for unknown routes
+        hmr: true,
+    },
 });
