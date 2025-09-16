e4737b0e6304ab6842c1f8959eb4d4580e2b9678
SLNR <sigurrinnan@gmail.com>
2025-09-16 12:03:51 +0200

    updated style

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <footer>
+            <div className="wrapper">
+                <p>
+                    Kontakt: <a href="mailto:basunister@gmail.com">basunister@gmail.com</a>
+                </p>
+            </div>
+        </footer>
+    );
+}
