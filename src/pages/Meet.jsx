88ab59cc7cfdece17659b7ef8acf3fbf1b949c07
SLNR <sigurrinnan@gmail.com>
2025-11-30 18:15:39 +0100

    Changed order of trombonists

--- src/pages/Meet.jsx
+++ src/pages/Meet.jsx
@@ -2,6 +2,12 @@ import Card from "../components/Card";
 
 export const musicians = [
     {
+        name: "Thoke Harada Sørensen",
+        description: `Hej, mit navn er Thoke, og jeg spiller tenorbasun i Træk og Toner. Jeg startede med at spille basun på Odense Musikskole. Derefter startede jeg i Tivoli-Garden og spillede der i 5 år med Kasper Thaarup som lærer. Lige nu spiller jeg på MGK-København med Andreas Olsen som lærer.`,
+        imgSrc: "/img/ths.jpg",
+        imgAlt: "Thoke, tenorbasunist i Træk og Toner",
+    },
+    {
         name: "Carl-Valdemar Meyer Nielsen",
         description: `Hej, jeg hedder Carl-Valdemar Meyer Nielsen. Jeg er 15 år og spiller tenorbasun i Træk og Toner. Jeg spiller inde i Tivoli-Garden til dagligt, og får undervisning af Kasper Thaarup. Jeg har gennem tiden deltaget i konkurrencer ved Det kongelige Danske Musikkonservatorium. Jeg har haft mange musikalske oplevelser. Jeg har været solist med Livgardens Musikkorps, spillet i Det gamle Radiohus Koncertsal og mange andre.`,
         imgSrc: "/img/cvmn.jpeg",
@@ -18,12 +24,6 @@ export const musicians = [
         description: `Hej, mit navn er Johannes. Jeg er 14 år gammel og spiller basbasun i basunkvartetten Træk og Toner. Derudover spiller jeg til dagligt i Tivoli-Garden, hvor jeg bl.a. studerer under solobasunist Kasper Thaarup, som skærper mine musikalske færdigheder.`,
         imgSrc: "/img/jtr.jpg",
         imgAlt: "Johannes, basbasunist i Træk og Toner",
-    },
-    {
-        name: "Thoke Harada Sørensen",
-        description: `Hej, mit navn er Thoke, og jeg spiller tenorbasun i Træk og Toner. Jeg startede med at spille basun på Odense Musikskole. Derefter startede jeg i Tivoli-Garden og spillede der i 5 år med Kasper Thaarup som lærer. Lige nu spiller jeg på MGK-København med Andreas Olsen som lærer.`,
-        imgSrc: "/img/ths.jpg",
-        imgAlt: "Thoke, tenorbasunist i Træk og Toner",
     },
 ];
 
